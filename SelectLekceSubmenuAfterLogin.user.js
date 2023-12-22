4143a62ea061bbdc039b8eb127959e0579e49d1f
kofaysi <17022173+kofaysi@users.noreply.github.com>
2023-12-22 15:59:18 +0100

    Update SelectLekceSubmenuAfterLogin.user.js
    changing function to load page B after loading to A

--- SelectLekceSubmenuAfterLogin.user.js
+++ SelectLekceSubmenuAfterLogin.user.js
@@ -11,30 +11,16 @@
 (function() {
     'use strict';
 
-    // Function to select the Lekce submenu
-    function selectLekceSubmenu() {
-        // Wait for the page to load
-        window.addEventListener('load', () => {
-            // Select the Lekce submenu
-            // We are using the href attribute to precisely select the 'Lekce' link
-            const lekceMenu = document.querySelector('a[href="detail_lide.php?zalozka=lekce"]');
-            if (lekceMenu) {
-                lekceMenu.click();
-            }
-        });
+    // Function to redirect to the Lekce submenu
+    function redirectToLekce() {
+        const currentUrl = window.location.href;
+        const targetUrl = 'https://lavoda.auksys.cz/master/detail_lide.php?zalozka=lekce';
+
+        if (currentUrl === 'https://lavoda.auksys.cz/master/') {
+            window.location.href = targetUrl;
+        }
     }
 
-    // Check if the user is logged in
-    // Implement the actual logic to determine if the user is logged in
-    // This example uses a placeholder function and should be replaced with actual logic
-    const isLoggedIn = () => {
-        // Replace with actual logic to determine if the user is logged in
-        // For example, you might check for a specific cookie or a user profile element
-        return document.cookie.includes('loggedIn=true'); // Placeholder
-    };
-
-    // If the user is logged in, select the Lekce submenu
-    if (isLoggedIn()) {
-        selectLekceSubmenu();
-    }
+    // Call the redirect function
+    redirectToLekce();
 })();
